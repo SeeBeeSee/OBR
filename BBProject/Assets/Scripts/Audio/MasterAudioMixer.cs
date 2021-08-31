e6d95fed6674da25222a468adfb93531101d15de
Kyle Hooks <contact@kyledhooks.com>
2021-08-31 12:13:25 -0500

    Added sliders to manipulate audio & playerPrefs

---
+++ BBProject/Assets/Scripts/Audio/MasterAudioMixer.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+
+public class MasterAudioMixer : MonoBehaviour
+{
+    public AudioMixer mixer;
+
+    private void Awake()
+    {
+        
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (!PlayerPrefs.HasKey("MasterVol"))
+        {
+            PlayerPrefs.SetFloat("MasterVol", .7f);
+        }
+        SetMasterLevel(PlayerPrefs.GetFloat("MasterVol"));
+
+
+        if (!PlayerPrefs.HasKey("MusicVol"))
+        {
+            PlayerPrefs.SetFloat("MusicVol", .7f);
+        }
+        SetMusicLevel(PlayerPrefs.GetFloat("MusicVol"));
+
+
+        if (!PlayerPrefs.HasKey("SfxVol"))
+        {
+            PlayerPrefs.SetFloat("SfxVol", .7f);
+        }
+        SetSfxLevel(PlayerPrefs.GetFloat("SfxVol"));
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void SetMasterLevel(float sliderValue)
+    {
+        mixer.SetFloat("MasterVol", Mathf.Log10(sliderValue) * 20);
+        PlayerPrefs.SetFloat("MasterVol", sliderValue);
+        Debug.Log(PlayerPrefs.GetFloat("MasterVol"));
+    }
+
+    public void SetMusicLevel(float sliderValue)
+    {
+        mixer.SetFloat("MusicVol", Mathf.Log10(sliderValue) * 20);
+        PlayerPrefs.SetFloat("MusicVol", sliderValue);
+    }
+
+    public void SetSfxLevel(float sliderValue)
+    {
+        mixer.SetFloat("SfxVol", Mathf.Log10(sliderValue) * 20);
+        PlayerPrefs.SetFloat("SfxVol", sliderValue);
+    }
+}
