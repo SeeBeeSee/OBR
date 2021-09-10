95973749012789094627212e8385e0c211309e08
Kyle Hooks <contact@kyledhooks.com>
2021-09-09 19:51:05 -0500

    Began implementing more bullet attacks, discovered timing issue with iTween/PM/Koreo

---
+++ BBProject/Assets/Scripts/Attacks/NBulletSpawnPattern.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using SonicBloom.Koreo;
+
+public class NBulletSpawnPattern : MonoBehaviour
+{
+    // Event string this SpawnPattern is listening for
+    public string spawnTrigger;
+    public List<Transform> bulletLocations;
+    PlayMakerFSM fsm;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        fsm = GetComponent<PlayMakerFSM>();
+        Koreographer.Instance.RegisterForEvents(spawnTrigger, Spawn);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void Spawn(KoreographyEvent e)
+    {
+        if (gameObject.activeSelf && bulletLocations.Count > 0)
+        {
+            foreach (Transform t in bulletLocations)
+            {
+                var newBullet = BulletPoolManager.instance.RequestSimpleBullet();
+                if (newBullet != null)
+                {
+                    newBullet.transform.position = t.position;
+                    //newBullet.transform.rotation = t.rotation;
+                    var rot = t.eulerAngles;
+                    newBullet.transform.rotation = Quaternion.Euler(rot);
+                    Debug.Log(t.rotation.eulerAngles + " " + newBullet.transform.rotation.eulerAngles);
+                }
+
+            }
+            fsm.SendEvent("Spawn");
+        }
+    }
+}
