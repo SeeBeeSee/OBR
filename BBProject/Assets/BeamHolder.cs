8619f04700b5d6d8e87962e2ff36ebbd7cc6654b
Kyle Hooks <contact@kyledhooks.com>
2021-09-19 19:07:49 -0500

    Beam attacks now spawn off-screen and move inwards during warning period

---
+++ BBProject/Assets/BeamHolder.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using PlayMaker;
+
+public class BeamHolder : MonoBehaviour
+{
+    public GameObject spawn;
+    public float spawnRadius;
+    public int spawnCount;
+    GameObject[] beams;
+    PlayMakerFSM fsm;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        beams = new GameObject[spawnCount];
+
+        for (int i=0; i<spawnCount; i++)
+        {
+            var angle = i * (2*Mathf.PI) / spawnCount;
+            var x = Mathf.Cos(angle) * spawnRadius;
+            var z = Mathf.Sin(angle) * spawnRadius;
+            Vector3 spawnPos = transform.position + new Vector3(x, 0, z);
+            float degrees = (-angle * Mathf.Rad2Deg) - 90;
+            Quaternion rotation = Quaternion.Euler(0, degrees, 0);
+            var newSpawn = Instantiate(spawn, spawnPos, rotation);
+            newSpawn.transform.parent = transform;
+            beams[i] = newSpawn;
+        }
+
+        fsm = GetComponent<PlayMakerFSM>();
+        fsm.FsmVariables.GetFsmArray("beams").Values = beams;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
