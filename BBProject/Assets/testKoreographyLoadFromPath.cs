df3fea2f9a6f5a94ac63f62fcd9d344da7de1d27
Kyle Hooks <contact@kyledhooks.com>
2021-09-27 15:27:43 -0500

    Started work on dynamically loading Koreography tracks at runtime

---
+++ BBProject/Assets/testKoreographyLoadFromPath.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using SonicBloom.Koreo;
+using SonicBloom.Koreo.Players;
+
+public class testKoreographyLoadFromPath : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    public string koreoPath;
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void LoadKoreoFromResources(SimpleMusicPlayer musicPlayer)
+    {
+        Debug.Log(koreoPath);
+        var track = Resources.Load(koreoPath);
+        if (track != null && track.GetType() == typeof(Koreography))
+        {
+            Debug.Log(track.GetType());
+            musicPlayer.LoadSong((Koreography)track, 0, false);
+        }
+        else Debug.LogError("Invalid path: " + koreoPath);
+    }
+}
